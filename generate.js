e3b23e7fb68a7cc129bd307b2929efc457ec9bb2
pi1r <vandal18pioneer@gmail.com>
2021-10-02 23:38:57 +0300

    more questions, now, it is not so silly::

--- generate.js
+++ generate.js
@@ -1,37 +1,70 @@
-var first = "Come on... Take her out on a date freak!";
-var second = "You can only communicate with your computer. Do not even dream";
-var third = "Firstly, look at the mirror.Maybe u need more beautiful girl? ^^";
-// добавили переменные
-
-const variant = [first, second, third] 
-// поместили их в массив 
+console.log("   Hey there!");
+console.log("");
+console.log("   Do you know whether to ask a girl on a date or not? Do you doubt your chances?");
+console.log("   Maybe she likes you? Or maybe not.");
+console.log(""); 
+console.log("   I'll help you, let's figure it out."); 
+console.log(""); 
+console.log("   Always answer  'yes' or 'no'"); 
 
 const prompt = require('prompt-sync')();
  
-const question1 = prompt('do you communicate a lot? (enter yes or no)');
+const question1 = prompt('   yes? ');
 
-if (question1 == "yes"){
-    variant.push(first);
-}
-else if (question1 == "no"){
-    variant.push(second)
-}
-else{
-    alert("freak"); 
-}
+var first = "   You have every chance, just do it!"; 
+var second = "   Take the risk, but be prepared for the worst";
 
-const question2 = prompt('do you often go out together?');
+const health = [first, second]; 
 
 
-if (question2 == "yes"){
-    variant.push(first);
-}
-else if (question2 == "no"){
-    variant.push(second)
-}
-else{
-    console.log("STOP!"); 
-}
-const random = Math.floor(Math.random()* variant.length);
-console.log(variant[random],);
+console.log("   So...First question");
 
+
+
+const qw1 = prompt("   Do you communicate a lot? ");
+
+if (qw1 == "yes"){
+    health.push(first);  // + 1 point
+}
+else if (qw1 == "no"){
+    health.push(second); // - 1 point
+};
+
+const qw2 = prompt("   Do you often go out together? ");
+
+if (qw2 == "yes"){
+    health.push(first);  // + 1 point
+}
+else if (qw2 == "no"){
+    health.push(second); // - 1 point
+};
+
+const qw3 = prompt("   Do you cheer her up? ");
+if (qw3 == "yes"){
+    health.push(first, first, first);  // + 3 point
+}
+else if (qw3 == "no"){
+    health.push(second, second); // - 2 point
+};
+
+const qw4 = prompt("   Does she feel comfortable with you? "); 
+if (qw4 == "yes"){
+    health.push(first, first, first);  // + 3 point
+}
+else if (qw4 == "no"){
+    health.push(second, second, second); // - 3 point
+};
+
+const qw5 = prompt("   Do you have common interests? ");
+if (qw5 == "yes"){
+    health.push(first, first);  // + 2 point
+}
+else if (qw5 == "no"){
+    health.push(second, second); // - 2 point
+};
+
+console.log("") // mooore space 
+console.log("") // mooore space 
+
+const random = Math.floor(Math.random()* health.length);
+console.log(health[random],);
